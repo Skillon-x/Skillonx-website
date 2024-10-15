af9b4a58386202c195193a373ab3ee9437b818ca
GitShailendra <kumarshailrndra7355@gmail.com>
2024-10-15 16:38:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ app.use(
       "http://localhost:5173",
       "https://skillonx.com",
       "https://skillonx.com/",
+      "https://lucky-yeot-c08cc8.netlify.app/"
     ],
     // methods:['GET','POST'],
     credentials: true,
